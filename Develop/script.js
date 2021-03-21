71494f0915641b285525b3c2981ed24bf3223046
BRASS <mr.brn.lewis@outlook.com>
2021-03-21 22:06:00 +0800

    Logic on how to ensure all criteria met created in JS

--- Develop/script.js
+++ Develop/script.js
@@ -16,24 +16,113 @@
 var generateBtn = document.querySelector("#generate");
 
 
+function userInput()
+{
+    var passLen = prompt("Password length? (8-128)");
+    var hasUpper = prompt("Uppercase? (Yes or No)");
+    var hasSpecial = prompt("Include special characters? (Yes or No)");
+    console.log(generateString())
+}
 
 
 // Write password to the #password input
 function writePassword() {
-    prompt("Number of chars?");
+
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to 'generateBtn'
+generateBtn.addEventListener("click", userInput);
 
-function generatePassword()
+
+function passwordBluePrint()
+{
+
+
+var test = ['1','2','3','4','1','2','3','4'];
+var returnArr = [];
+var genNum;
+var position = 1;
+
+do  
+{
+
+genNum = Math.floor(Math.random()* 8)+1;
+
+if (test[genNum-1] !== null )
+{
+console.log("if", genNum);
+returnArr.push(genNum);
+test[genNum-1] = null;
+}
+
+}
+while(returnArr.length < 8);
+return returnArr;
+}
+
+console.log(passwordBluePrint());
+
+
+function generateString()
+{
+    
+var random_string = '';
+var random_ascii;
+
+//lower case
+var ascii_low = 97; 
+var ascii_high = 122;
+
+//upper case
+var ascii_upper_low = 65;
+var ascii_upper_high = 90;
+
+//numbers
+var ascii_number_low = 49;
+var ascii_number_high = 57;
+
+//special_characters
+var ascii_special_low = 33;
+var ascii_special_high = 47;
+var allUsed = false;
+
+
+switch (math.floor((math.random()) * 4))
 {
 
 }
 
 
 
+
+
+random_ascii = Math.floor((Math.random() * (ascii_high - ascii_low)) + ascii_low);
+random_string = String.fromCharCode(random_ascii)
+
+return random_string
+
+}
+
+function caseUpper()
+{
+
+}
+
+function caseLower()
+{
+
+}
+
+function numberGen()
+{
+
+}
+
+function symGen()
+{
+
+}
